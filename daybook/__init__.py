e8c8dbdb3c9208bad6fb1eca7403083dfbc388fe
Jason LeCount <jlecount@gmail.com>
2020-05-26 08:48:51 -0700

    --until or --since must come before base dir for git log

--- daybook/__init__.py
+++ daybook/__init__.py
@@ -81,11 +81,14 @@ class Daybook(object):
         
         :return: a set of files between two dates.  The format should be that which can be parsed by git.
         """
-        cmd = ['git', 'log', '--name-only', '--author-date-order', '--pretty=%b', '.']
+        cmd = ['git', 'log', '--name-only', '--author-date-order', '--pretty=%b']
         if before_date:
             cmd += [f"--until={before_date}"]
         if after_date:
             cmd += [f"--since={after_date}"]
+
+        # add current dir
+        cmd += '.'
 
         raw = subprocess.check_output(cmd, cwd=self.project_dir).decode('ascii').split('\n')
 
